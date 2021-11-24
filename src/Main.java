094a073825dd10b04ccc87006f7785da9fe3d53a
Igor Meleshko <igor.meleshko.1109@gmail.com>
2021-11-24 19:48:45 +0300

    commit mili

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 100000;
+        int pricePerMile = 20;
+        int numberMiles = ticketPrice / pricePerMile;
+        System.out.println(numberMiles +" miles");
+    }
+}
+
